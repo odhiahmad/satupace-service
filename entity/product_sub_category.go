185def0338c8a5d0e46185cc861d043fda39e72d
odhahmad <odhiahmad15@gmail.com>
2024-11-20 23:23:58 +0700

    :wrench: fixing db

--- entity/product_sub_category.go
+++ entity/product_sub_category.go
@@ -1,6 +1,8 @@
 package entity
 
 type ProductSubCategory struct {
-	ID   int    `gorm:"type:int;primary_key"`
-	Name string `gorm:"type:varchar(255)" json:"name"`
+	ID                int `gorm:"type:int;primary_key"`
+	ProductCategoryID uint
+	ProductCategory   ProductCategory `gorm:"foreignKey:ProductCategoryID"`
+	Name              string          `gorm:"type:varchar(255)" json:"name"`
 }
