3c00eb86ec997cfe42e81f9c921fe1b9c730f8d1
Odhi Ahmad <odhiahmad15@gmail.com>
2025-07-16 18:58:19 +0700

    fixing

--- data/request/user_req.go
+++ data/request/user_req.go
@@ -2,18 +2,18 @@ package request
 
 type UserUpdateDTO struct {
 	ID       int
-	Email    string `json:"email" form:"email" binding:"required"`
+	Email    string `json:"email" form:"email"`
 	Password string `json:"password,omitempty" form:"password,omitempty" binding:"required" validate:"min:6"`
 }
 
 type UserCreateDTO struct {
-	Nama     string `json:"nama" form:"nama" binding:"required"`
+	Nama     string `json:"nama" form:"nama"`
 	Email    string `json:"email" form:"email" binding:"required"`
 	Password string `json:"password,omitempty" form:"password,omitempty" binding:"required" validate:"min:6"`
 }
 
 type ProfileRequest struct {
-	Id             int     `json:"id" binding:"required"`                     // ID diperlukan untuk update                    // opsional                  // opsional                 // opsional
+	Id             int     `json:"id"`                                        // ID diperlukan untuk update                    // opsional                  // opsional                 // opsional
 	Email          *string `json:"email,omitempty" binding:"omitempty,email"` // opsional dan valid email
 	PhoneNumber    *string `json:"phone_number,omitempty"`                    // opsional                  // opsional                  // opsional
 	BusinessName   *string `json:"business_name,omitempty"`
@@ -27,13 +27,13 @@ type ProfileRequest struct {
 }
 
 type ChangePasswordRequest struct {
-	Id          int    `json:"id" binding:"required"`
+	Id          int    `json:"id"`
 	OldPassword string `json:"old_password" binding:"required"`
 	NewPassword string `json:"new_password" binding:"required,min=6"`
 }
 
 type ChangeEmailRequest struct {
-	Id    int     `json:"id" binding:"required"`
+	Id    int     `json:"id"`
 	Email *string `json:"email" binding:"required,email"`
 }
 
