75840f6c2dc775a92a7fc08a80f362a9e528e365
Odhi Ahmad <odhiahmad15@gmail.com>
2025-07-20 10:16:02 +0700

    fixing

--- data/request/discount_req.go
+++ data/request/discount_req.go
@@ -3,16 +3,15 @@ package request
 import "time"
 
 type DiscountRequest struct {
-	BusinessId   int       `json:"business_id" validate:"required"`
-	Name         string    `json:"name" validate:"required"`
-	Description  string    `json:"description"`   // baru
-	IsPercentage bool      `json:"is_percentage"` // baru
-	Amount       float64   `json:"amount" validate:"required"`
-	IsGlobal     bool      `json:"is_global"`
-	IsMultiple   bool      `json:"is_multiple"` // baru
-	StartAt      time.Time `json:"start_at" validate:"required"`
-	EndAt        time.Time `json:"end_at" validate:"required"`
-	IsActive     bool      `json:"is_active"` // baru
+	BusinessId  int       `json:"business_id" validate:"required"`
+	Name        string    `json:"name" validate:"required"`
+	Description string    `json:"description"`
+	Amount      float64   `json:"amount" validate:"required"`
+	IsGlobal    bool      `json:"is_global"`
+	IsMultiple  bool      `json:"is_multiple"`
+	StartAt     time.Time `json:"start_at" validate:"required"`
+	EndAt       time.Time `json:"end_at" validate:"required"`
+	IsActive    bool      `json:"is_active"`
 }
 
 type DiscountIsGlobal struct {
