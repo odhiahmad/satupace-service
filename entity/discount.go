cbb0f2bfd824f558a018eeb553aecf1c56678f67
Odhi Ahmad <odhiahmad15@gmail.com>
2025-07-21 21:02:59 +0700

    fixing entity discount

--- entity/discount.go
+++ entity/discount.go
@@ -9,11 +9,11 @@ type Discount struct {
 	Description  string    `json:"description"`
 	IsPercentage *bool     `gorm:"not null;default:false" json:"is_percentage"` // true = amount sebagai persen
 	Amount       float64   `gorm:"not null" json:"amount"`                      // nilai diskon
-	IsGlobal     *bool     `gorm:"not null" json:"is_global"`                   // true = berlaku untuk semua produk
+	IsGlobal     *bool     `gorm:"not null;default:false" json:"is_global"`     // true = berlaku untuk semua produk
 	IsMultiple   *bool     `gorm:"not null;default:false" json:"is_multiple"`   // true = berlaku kelipatan (misal beli 2x, diskon 2x)
 	StartAt      time.Time `json:"start_at"`
 	EndAt        time.Time `json:"end_at"`
-	IsActive     *bool     `gorm:"not null" json:"is_active"`
+	IsActive     *bool     `gorm:"not null;default:false" json:"is_active"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
